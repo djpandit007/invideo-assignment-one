b3cc966b810bad58dc5e5d8d80d310cce14f54de
Dhananjay <dj.pandit007@gmail.com>
2025-01-30 23:32:51 +0530

    Fix some CSS and rsx

--- src/views/shader.rs
+++ src/views/shader.rs
@@ -5,18 +5,31 @@ const SHADER_CSS: Asset = asset!("/assets/styling/shader.css");
 
 #[component]
 pub fn Shader() -> Element {
+    let mut text_prompt = use_signal(|| String::new());
+    let mut shader_response = use_signal(|| String::new());
+
     rsx! {
         document::Link { rel: "stylesheet", href: SHADER_CSS }
 
         div {
             id: "shader",
-            h4 { "Enter a mathematical expression" }
+            h4 { "Text to Shader" }
             input {
-                placeholder: "Type here to calculate...",
-                oninput:  move |event| async move {},
+                placeholder: "A rotating cube with a gradient background",
+                oninput:  move |event| text_prompt.set(event.value()),
             }
-            button { id: "generate", "Generate"}
+            button {
+                id: "generate",
+                onclick: move |_| async move {
+                    let value = crate::backend::call_openai(text_prompt.to_string()).await;
+                    shader_response.set(value.unwrap());
+                },
+                "Generate"
+            }
 
+            div {
+                pre { "{shader_response}" }
+            }
         }
     }
 }
